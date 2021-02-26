91adba8ef945cea3bae0273038fa53fa55f9ab79
Barnes, Dan <dbarnes1@paychex.com>
2021-02-26 13:22:57 -0500

    fix: replace lodash-es with lodash in cjs

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const { terser } = require("rollup-plugin-terser");
 const polyfills = require('rollup-plugin-node-polyfills');
 const commonjs = require('@rollup/plugin-commonjs');
+const replace = require('@rollup/plugin-replace');
 const { babel } = require("@rollup/plugin-babel");
 
 const pkg = require('./package.json');
@@ -33,28 +34,42 @@ module.exports = [
             sourcemap: true,
         },
     },
-    // ESM and CJS
+    // ESM
     {
         input: 'index.mjs',
+        external: ['lodash-es', '@paychex/core'],
         plugins: [
             nodeResolve(),
             commonjs({
                 include: /node_modules/,
             })
         ],
-        output: [
-            {
-                dir: "dist/esm",
-                format: "esm",
-                exports: "named",
-                sourcemap: true,
-            },
-            {
-                dir: "dist/cjs",
-                format: "cjs",
-                exports: "named",
-                sourcemap: true,
-            },
+        output: {
+            dir: "dist/esm",
+            format: "esm",
+            exports: "named",
+            sourcemap: true,
+        },
+    },
+    // CJS
+    {
+        input: 'index.mjs',
+        external: ['lodash', '@paychex/core'],
+        plugins: [
+            replace({
+                'lodash-es': 'lodash',
+                preventAssignment: true,
+            }),
+            nodeResolve(),
+            commonjs({
+                include: /node_modules/,
+            })
         ],
+        output: {
+            dir: "dist/cjs",
+            format: "cjs",
+            exports: "named",
+            sourcemap: true,
+        },
     },
 ];
